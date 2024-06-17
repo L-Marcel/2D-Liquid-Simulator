d682fc15a26ec11b6be6375d8c4c030171ded007
L-Marcel <lmgh1312@gmail.com>
2024-06-17 14:26:02 -0300

    Finishing version to Godot 4.3 and writing README.

--- addons/liquidsimulator/LiquidSimulator.gd
+++ addons/liquidsimulator/LiquidSimulator.gd
@@ -2,9 +2,9 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	add_custom_type("Liquid", "Node2D", preload("Liquid.gd"), preload("icons/Liquid.svg"));
-	add_custom_type("LiquidServer", "Node", preload("LiquidServer.gd"), preload("icons/LiquidServer.svg"));
-	add_custom_type("LiquidMap", "Resource", preload("LiquidMap.gd"), preload("icons/LiquidMap.svg"));
+	add_custom_type("Liquid", "Node2D", preload("Liquid.gd"), preload("./icons/Liquid.svg"));
+	add_custom_type("LiquidServer", "Node", preload("LiquidServer.gd"), preload("./icons/LiquidServer.svg"));
+	add_custom_type("LiquidMap", "Resource", preload("LiquidMap.gd"), preload("./icons/LiquidMap.svg"));
 
 func _exit_tree():
 	remove_custom_type("Liquid");
